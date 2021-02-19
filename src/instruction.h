8a2a7e5cf21be21fd379a4e9635d4e20dee063b1
ctfloyd <ctfloyd@wisc.edu>
2021-02-18 23:30:04 -0600

    Factor instructions addressing modes

--- src/instruction.h
+++ src/instruction.h
@@ -13,6 +13,19 @@ enum Instruction {
     LDA_ABS_X = 0xBD
 };
 
+static inline Byte load_zero_page_value(CPU* cpu, Byte offset) {
+    Byte zero_page_addr = cpu_load_next_byte(cpu);
+    // The size of Byte is u8, so this wraps at 255
+    Byte effective_addr = zero_page_addr + offset;
+    return cpu->memory.data[effective_addr];
+}
+
+static inline Byte load_absolute_value(CPU* cpu, Byte offset) {
+    Word base_addr = cpu_load_next_word(cpu);
+    Word effective_addr = base_addr + offset;
+    return cpu->memory.data[effective_addr];
+}
+
 static inline int lda_imm(CPU* cpu) {
     // Sets zero and negative flags
     Byte accumulator_byte = cpu_load_next_byte(cpu);
@@ -22,44 +35,31 @@ static inline int lda_imm(CPU* cpu) {
 }
 
 static inline int lda_zero(CPU* cpu) {
-    Byte zero_page_addr = cpu_load_next_byte(cpu);
-    Byte accumulator_byte = cpu->memory.data[zero_page_addr];
+    Byte accumulator_byte = load_zero_page_value(cpu, 0);
     cpu->accumulator = accumulator_byte;
     flags_set_nz(&cpu->flags, accumulator_byte);
     return 3;
 }
 
 static inline int lda_zero_x(CPU* cpu) {
-    Byte zero_page_addr = cpu_load_next_byte(cpu);
-    Byte offset = cpu->idx_reg_x;
-
-    // The size of Byte is u8, so this wraps at 255
-    Byte effective_addr = zero_page_addr + offset;
-    Byte accumulator_byte = cpu->memory.data[effective_addr];
+    Byte accumulator_byte = load_zero_page_value(cpu, cpu->idx_reg_x);
     cpu->accumulator = accumulator_byte;
     flags_set_nz(&cpu->flags, accumulator_byte);
     return 4;
 }
 
 static inline int lda_absolute(CPU* cpu) {
-    Word effective_addr = cpu_load_next_word(cpu);
-    Byte accumulator_byte = cpu->memory.data[effective_addr];
-    cpu->accumulator = accumulator_byte;
-    flags_set_nz(&cpu->flags, accumulator_byte);
-    return 4;
-}
-
-static inline int lda_absolute_with_offset(CPU* cpu, Byte offset) {
-    Word base_addr = cpu_load_next_word(cpu);
-    Word effective_addr = base_addr + offset;
-    Byte accumulator_byte = cpu->memory.data[effective_addr];
+    Byte accumulator_byte = load_absolute_value(cpu, 0);
     cpu->accumulator = accumulator_byte;
     flags_set_nz(&cpu->flags, accumulator_byte);
     return 4;
 }
 
 static inline int lda_absolute_x(CPU* cpu) {
-    return lda_absolute_with_offset(cpu, cpu->idx_reg_x);
+    Byte accumulator_byte = load_absolute_value(cpu, cpu->idx_reg_x);
+    cpu->accumulator = accumulator_byte;
+    flags_set_nz(&cpu->flags, accumulator_byte);
+    return 4;
 }
 
 #endif
